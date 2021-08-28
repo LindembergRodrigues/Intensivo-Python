aa9507a452931218f3d1ef8a53a6bebd022a5c29
Lindemberg <bergfla2010@hotmail.com>
2021-08-28 02:18:30 -0300

    dicionario_de_dicionario.py

---
+++ dicionario_de_dicionario.py
@@ -0,0 +1,15 @@
+
+nomes = input("Nomes separados por vírgula: ").split(",")
+dic = {}
+for nome in nomes:
+    dic_aux = {}
+    print(nome)
+    dic_aux["sobrenome"] = input("Sobrenome: ")
+    dic_aux["idade"] = input("Idade: ")
+    dic_aux["endereco"] = input("Endereco: ")
+    dic[nome] = dic_aux
+
+for chaves in dic:
+
+    print ("{nome} {sobrenome}, tem {idade} anos e mora em {endereco}".format(nome=chaves,
+            sobrenome=dic[chaves]["sobrenome"],idade=dic[chaves]["idade"], endereco = dic[chaves]["endereco"] ))
