da5f54369be2404a5cbbdc0ff1821bdbf48c9530
Lindemberg <bergfla2010@hotmail.com>
2021-08-28 21:47:09 -0300

    arquivos testes

---
+++ outros/Ordena_nome.py
@@ -0,0 +1,24 @@
+# recebe uma lista com n nomes e remove todos que possuei mais de 3 vogais
+
+def verifica_nome(nome):
+    ctd = 0
+    for i in  nome:
+        if i in "aeiouAEIOU":
+            ctd += 1
+
+    if ctd > 3:
+        return True
+    else:
+        return False
+
+def lista_a_verificar(nomes):
+    for i in range(len(nomes)-1,-1,-1):
+        if verifica_nome(nomes[i]):
+            nomes.pop(i)
+    return nomes
+
+lista_nomes = input("Informe os nomes a serem verificados: \nOs nomes devem ser preseguidos de vírgula e espaço"
+                    " (Ex:Fulano, Ciclano) :").split(", ")
+
+print("Lista após a remoção dos nomes que estão contidos na regra>>", lista_a_verificar(lista_nomes[:]))
+print("Lista inicial>>", lista_nomes)
